9750ead9368e832361fca8cafc43f90b785b3f44
Haley Wang <hw211@duke.edu>
2020-10-05 11:17:23 -0400

    set up

---
+++ web/src/main/java/web/usermanagement/DemoApp.java
@@ -0,0 +1,12 @@
+package web.usermanagement;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class DemoApp {
+    public static void main(String[] args) {
+    	SpringApplication.run(DemoApp.class, args);
+    }
+}
